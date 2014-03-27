d810446850c25cadb2978a6bf829d249d31672d8
alexs333 <aserdyuk@dius.com.au>
2014-03-27 17:47:02 +1100

    Initial Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocatorBoard::Application.config.secret_key_base = 'c4612333a71b7b9696a7cac2b1a112dea2a8253ff69db2b0c48389126d2fde6945a78481f641577e222b494edb0a403576b27b8e7be5a7cd943412564217d425'
