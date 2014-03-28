d7ba1c15340faffe8941d85a18ee9ea8aa9eaa96
dean <dnetherton@dius.com.au>
2014-03-28 11:45:39 +1100

    added data extracted from timesheets - raw form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+config = LocatorBoard::Application.config.database_configuration[::Rails.env]
+dbhost, dbuser, dbname = config['host'], config['username'], config['database']
+
+dbhost = "localhost"
+
+copy_command = "\\copy people_relationships (client_id, proj_id, user_id, client, project, username, removed) from '#{Rails.root}/db/people_relationships.csv' csv header;"
+sql_command = "psql -U #{dbuser} -h #{dbhost} #{dbname} -c \"#{copy_command}\""
+
+puts sql_command
+
+`#{sql_command}`
