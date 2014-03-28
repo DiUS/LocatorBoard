80dd08ba738c0e4e110347835ee5aaa29af1a542
dean <dnetherton@dius.com.au>
2014-03-28 12:22:17 +1100

    added basic user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328002623) do
+ActiveRecord::Schema.define(version: 20140328011858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20140328002623) do
     t.string   "project"
     t.string   "username"
     t.integer  "removed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
